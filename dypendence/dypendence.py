dff1b391f575ef21546cd70e175a28d5e5887360
VaultVulp <me@vaultvulp.dev>
2020-09-01 19:01:11 +0300

    Rework project

--- dypendence/dypendence.py
+++ dypendence/dypendence.py
@@ -1,4 +1,5 @@
 from dynaconf import Dynaconf
+from dynaconf.utils.boxing import DynaBox
 
 
 class DYException(Exception):
@@ -13,72 +14,45 @@ class DYNotFoundException(DYException):
 
 
 class DY:
-    GLOBAL_PREFIX = 'DY'
-    CLASS_PREFIX = None
-    TYPE_POSTFIX = 'TYPE'
-    settings = None
+    GLOBAL_PREFIX: str = 'dy'
+    FACTORY_PREFIX: str = None
+    TYPE_POSTFIX: str = 'type'
+
+    global_settings: Dynaconf = None
+    factory_settings: DynaBox = None
+
+    @classmethod
+    def _get_class_prefix(cls):
+        return None
 
     def __init_subclass__(cls, **kwargs):
-        print(f'Classname: {cls.__name__}')
-        cls.settings = DY.settings.get(cls.__name__)
-        # if not isinstance(cls.CLASS_PREFIX, str):
-        #     raise DYException(f'Class-level variable `PREFIX` in class {cls.__name__} must be a dot separated path')
+        if cls.FACTORY_PREFIX is None:
+            cls.FACTORY_PREFIX = cls.__name__
 
     def __new__(cls, **dynaconf_kwargs):
-        global_settings = Dynaconf(**dynaconf_kwargs)
-        cls.settings = settings = global_settings.get(f'{cls.GLOBAL_PREFIX}.{cls.CLASS_PREFIX}')
+        cls.global_settings: Dynaconf = Dynaconf(**dynaconf_kwargs)
+        cls.factory_settings: DynaBox = cls.global_settings.get(f'{cls.GLOBAL_PREFIX}.{cls.FACTORY_PREFIX}')
+
+        if cls.factory_settings is None:
+            path = f'{cls.GLOBAL_PREFIX}.{cls.FACTORY_PREFIX}'
+            raise DYException(f'Invalid configuration structure: Key `{path.upper()}` not present')
+
+        if not cls.factory_settings.get(cls.TYPE_POSTFIX):
+            path = f'{cls.GLOBAL_PREFIX}.{cls.FACTORY_PREFIX}.{cls.TYPE_POSTFIX}'
+            raise DYException(f'Invalid configuration structure: Key `{path.upper()}` not present')
+
+        requested_classname = str(cls.factory_settings.get(cls.TYPE_POSTFIX)).upper()
 
         subclasses = {klass.__name__.upper(): klass for klass in cls.__subclasses__()}
 
-        if not settings.get(cls.TYPE_POSTFIX):
-            raise DYException(
-                f'Invalid configuration structure: '
-                f'Key `{cls.GLOBAL_PREFIX}.{cls.CLASS_PREFIX}.{cls.TYPE_POSTFIX}` not present'
-            )
-
-        classname = str(settings.get(cls.TYPE_POSTFIX)).upper()
-
-        if classname not in subclasses:
+        if requested_classname not in subclasses:
             raise DYNotFoundException(
-                dependency_name=classname,
+                dependency_name=requested_classname,
                 allowed_values=', '.join(subclasses.keys()),
             )
-        return super().__new__(subclasses[classname])
-
-
-class DootFactory(DY):
-    CLASS_PREFIX = 'DOOT'
-
-    def __new__(cls, *args, **kwargs):
-        return super(DootFactory, cls).__new__(cls, *args, **kwargs)
+        return super().__new__(subclasses[requested_classname])
 
     @property
-    def value(self):
-        raise NotImplemented
-
-
-class Dooter(DootFactory):
-
-    @property
-    def value(self):
-        return 'Dooter'
-
-
-class Buzz(DootFactory):
-
-    @property
-    def value(self):
-        return 'Buzz'
-
-
-class Fizz(DootFactory):
-
-    @property
-    def value(self):
-        return 'Fizz'
-
-
-if __name__ == '__main__':
-    something = DootFactory(loaders=['dynaconf.loaders.env_loader'])
-    print(f'Type: {type(something).__name__}')
-    print(something.value)
+    def settings(self) -> DynaBox:
+        classname = type(self).__name__
+        return self.factory_settings.get(classname, DynaBox(box_settings={}))
--- dypendence/tests/test_dypendence.py
+++ dypendence/tests/test_dypendence.py
@@ -1,7 +1,92 @@
-import pytest
-
-pytestmark = pytest.mark.asyncio
+from dypendence.dypendence import DY
 
 
-async def test_main():
-    assert True
+class FizzBuzz(DY):
+    FACTORY_PREFIX = 'SOME_DIFFERENT_PATH'
+
+    @property
+    def value(self) -> str:
+        raise NotImplementedError
+
+    @property
+    def value_from_settings(self) -> str:
+        raise NotImplementedError
+
+
+class Buzz(FizzBuzz):
+
+    @property
+    def value(self) -> str:
+        return 'Buzz Value'
+
+    @property
+    def value_from_settings(self) -> str:
+        return self.settings.settings_value
+
+
+class Fizz(FizzBuzz):
+
+    @property
+    def value(self) -> str:
+        return 'Fizz Value'
+
+    @property
+    def value_from_settings(self) -> str:
+        return self.settings.settings_value
+
+
+def test_buzz_constructor(monkeypatch):
+    monkeypatch.setenv('DYNACONF_DY__SOME_DIFFERENT_PATH__FIZZ__SETTINGS_VALUE', 'Fizz from dynaconf settings')
+    monkeypatch.setenv('DYNACONF_DY__SOME_DIFFERENT_PATH__BUZZ__SETTINGS_VALUE', 'Buzz from dynaconf settings')
+    monkeypatch.setenv('DYNACONF_DY__SOME_DIFFERENT_PATH__TYPE', 'Buzz')
+
+    buzz = FizzBuzz(loaders=['dynaconf.loaders.env_loader'])
+
+    assert isinstance(buzz, Buzz)
+    assert buzz.value == 'Buzz Value'
+    assert buzz.value_from_settings == 'Buzz from dynaconf settings'
+
+
+def test_fizz_constructor(monkeypatch):
+    monkeypatch.setenv('DYNACONF_DY__SOME_DIFFERENT_PATH__FIZZ__SETTINGS_VALUE', 'Fizz from dynaconf settings')
+    monkeypatch.setenv('DYNACONF_DY__SOME_DIFFERENT_PATH__BUZZ__SETTINGS_VALUE', 'Buzz from dynaconf settings')
+    monkeypatch.setenv('DYNACONF_DY__SOME_DIFFERENT_PATH__TYPE', 'Fizz')
+
+    fizz = FizzBuzz(loaders=['dynaconf.loaders.env_loader'])
+
+    assert isinstance(fizz, Fizz)
+    assert fizz.value == 'Fizz Value'
+    assert fizz.value_from_settings == 'Fizz from dynaconf settings'
+
+
+class NotificationService(DY):
+
+    def send_notification(self) -> str:
+        raise NotImplementedError
+
+
+class SMSService(NotificationService):
+
+    def send_notification(self) -> str:
+        return 'Sent SMS Notification'
+
+
+class EmailService(NotificationService):
+
+    def send_notification(self) -> str:
+        return 'Sent Email Notification'
+
+
+class PushService(NotificationService):
+
+    def send_notification(self) -> str:
+        return 'Sent Push Notification'
+
+
+def test_notification_service_constructor(monkeypatch):
+    monkeypatch.setenv('DYNACONF_DY__NOTIFICATIONSERVICE__TYPE', 'SMSService')
+
+    sms_service = NotificationService(loaders=['dynaconf.loaders.env_loader'])
+
+    assert isinstance(sms_service, SMSService)
+    assert sms_service.send_notification() == 'Sent SMS Notification'
